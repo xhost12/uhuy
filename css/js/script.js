9f7420b012487b080e52646aa631a6a58a218e1d
xhost12 <149801243+xhost12@users.noreply.github.com>
2023-11-03 22:04:27 +0700

    Delete css/js/script.js

--- css/js/script.js
+++
@@ -1,6 +0,0 @@
-// toggle class active
-const navbarNav = document.querySelector("navbar-nav");
-
-document.querySelector(".#align-justify").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
