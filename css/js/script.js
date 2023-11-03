60c6c79c5cbdbb73b505b31e834968d9f02e66a1
xhost12 <149801243+xhost12@users.noreply.github.com>
2023-11-03 22:05:05 +0700

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,6 @@
+// toggle
+const navbarnav = document.querySelector(".navbar-nav");
+// toggle onlick
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarnav.classList.toggle("active");
+};
