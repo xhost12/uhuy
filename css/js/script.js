2a532d9069a855b4ba716c5d422416cf88cf1992
xhost12 <149801243+xhost12@users.noreply.github.com>
2023-11-03 23:17:48 +0700

    Delete css/js/script.js

--- css/js/script.js
+++
@@ -1,7 +0,0 @@
-// toggle
-const navbarNav = document.querySelector (.navbar-nav);
-// toggle onlick
-document.querySelector(#hamburger-menu).onclick =  () => {
-
-  navbarNav.classList.toggle(active);
-};
