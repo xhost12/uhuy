1bb667bed95d968c654cff78d8bff9c75af2b9e5
xhost12 <149801243+xhost12@users.noreply.github.com>
2023-11-03 21:29:34 +0700

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,6 @@
+// toggle class active
+const navbarNav = document.querySelector("navbar-nav");
+
+document.querySelector(".#align-justify").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
