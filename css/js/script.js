fe9a05d4847a687c87326dbc2d0229a4d1183e45
xhost12 <149801243+xhost12@users.noreply.github.com>
2023-11-04 13:07:31 +0700

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,7 +1,16 @@
 // toggle
-const navbarNav = document.querySelector (.navbar-nav);
+const navbarNav = document.querySelector(".navbar-nav");
 // toggle onlick
-document.querySelector(#hamburger-menu).onclick =  () => {
-
-  navbarNav.classList.toggle(active);
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
+
+// // close menu
+// const navbarNav = document.querySelector("#hamburger-menu");
+
+// document.addEventListener('click', function(e) {
+
+//   if (!hamburger.constains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove('active');
+//   }
+// });
